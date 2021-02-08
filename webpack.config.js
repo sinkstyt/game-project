559ffd307598b116dda7f66dbd84df8539ad038d
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-02-07 21:58:07 -0800

    Download dotenv webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-
+const Dotenv = require('dotenv-webpack');
 
 module.exports = {
   entry: './src/main.js',
@@ -14,6 +14,7 @@ module.exports = {
     contentBase: './dist'
   },
   plugins: [
+    new Dotenv(),
     new CleanWebpackPlugin(), 
     new HtmlWebpackPlugin({
       title: 'Shape Tracker',
