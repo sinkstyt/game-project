76b0b2c3d738b6fe7c4b3eb14de776ad393702fb
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-02-08 10:25:07 -0800

    Download file-loader and html-folder.

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,26 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         loader: "eslint-loader"
-      }
+      },
+      {
+        test: /\.(gif|png|jpe?g)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/images/'
+            }
+          }
+        ]
+      },
+      
+      {
+        test:/\.html$/,
+        use: [
+          'html-loader'
+        ]
+      },
     ]
   }
 };
