e9bd2366caf58ee6efde8e260ee97906f40a118c
Tyler Sinks <tyler.sinks@gmail.com>
2021-02-15 16:12:40 -0600

    achieve good fail initial Player keys
    Co-authored-by: Morgan Bradford <morganjbradford95@gmail.com>
    dquote> Co-authored-by: Logan Roth <diamondintheroth@gmail.com>
    dquote> Co-authored-by: Tien Nguyen <tien96ng@gmail.com>

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,11 +1,19 @@
 import Player from "../src/js/Player.js";
 
 describe("Player", () => {
-    
+  let newPlayer;
   
+  beforeEach(() => {
+    newPlayer = new Player("Gaston");
+  });
   
   test("should return a new instance of Player with player's name passed in upon instantiation.", () => {
-    const newPlayer = new Player("Jerry");
-    expect(newPlayer.name).toEqual("Jerry");
+    expect(newPlayer.name).toEqual("Gaston");
+  });
+
+  test("should return a new instance of Player with keys inventory, gold, and iron assigned to an empty map, 2, and 0 respectively", () => {
+    expect(newPlayer.inventory).toEqual({});
+    expect(newPlayer.gold).toEqual(2);
+    expect(newPlayer.iron).toEqual(0);
   });
 }); 
--- src/js/Player.js
+++ src/js/Player.js
@@ -1,6 +1,10 @@
 export default class Player {
   constructor(name) {
+    // this.name = name;
+    // this.inventory = new Map();
+    // this.gold = 2;
+    // this.iron = 0;
 
   }
-  
+
 }
