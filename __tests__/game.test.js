4f9bcd474c74dfb79af3c600b909892bdecb33e8
Tyler Sinks <tyler.sinks@gmail.com>
2021-02-15 13:39:11 -0600

    Edit game.test.js

--- __tests__/game.test.js
+++ __tests__/game.test.js
@@ -1,9 +1,8 @@
 import Game from "../src/js/game.js";
 
 describe("Game", () => {
-    test("Should", () => {
+    test("Should return a new game with player's name.", () => {
         const newGame = new Game("Tyler");
         expect(newGame.player).toEqual("Tyler");
     });
-  
-  }); 
+}); 
