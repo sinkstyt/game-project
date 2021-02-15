972f4d48f9e6692cc0d29d2af3bf8b5d6635213c
Tyler Sinks <tyler.sinks@gmail.com>
2021-02-15 17:17:35 -0600

    write, pass 0 health endGame spec
    Co-authored-by: Morgan Bradford <morganjbradford95@gmail.com>
    dquote> Co-authored-by: Logan Roth <diamondintheroth@gmail.com>
    dquote> Co-authored-by: Tien Nguyen <tien96ng@gmail.com>

--- __tests__/game.test.js
+++ __tests__/game.test.js
@@ -1,4 +1,5 @@
 import Game from "../src/js/game.js";
+import Player from "../src/js/Player.js";
 
 describe("Game", () => {
   let newGame;
@@ -7,7 +8,14 @@ describe("Game", () => {
     newGame = new Game("player1");
   });
 
-  test("should return false if the end game requirements aren't met", () => {
+  test("should return false if the end game requirements aren't met.", () => {
     expect(newGame.endGame()).toEqual(false);
+  });
+
+  test("should return true if the player's health reaches 0.", () => {
+    const player1 = new Player("Tyler");
+    player1.health = 0;
+    const newGame1 = new Game(player1);
+    expect(newGame1.endGame()).toEqual(true);
   });
 }); 
