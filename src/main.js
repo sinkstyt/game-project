9d7a1e04d575e4a75d08ea43d9aabb1a0ce4184a
Tyler Sinks <tyler.sinks@gmail.com>
2021-02-16 18:43:51 -0600

    add btn logic outline, h2s main options
    Co-authored-by: Morgan Bradford <morganjbradford95@gmail.com>

--- src/main.js
+++ src/main.js
@@ -5,11 +5,25 @@ import './css/styles.css';
 import Game from './js/game.js';
 import Player from './js/Player.js';
 
+
 $(document).ready(function() {
   // START PAGE KANBAN captures userName
   
   let player1 = new Player("Alexi");
   let game1 = new Game(player1);
-  
+
+  $('#venture').click(function() {
+    player1.venture();
+    // clearAdventureResults();
+    // displayAdventureResults();
+  });
+  $('#shop').click(function() {
+    mainMenu.hide()
+    $('#shop-menu').show();
+  });
+
+  $('#build').click(function() {
+
+  });
 
 });
